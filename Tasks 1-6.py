85ad275f6837b6062422def936afd4abf00d13c5
otisLITS <otis01990@gmail.com>
2017-07-09 22:44:41 +0300

    5 down 1 to go

--- Tasks 1-6.py
+++ Tasks 1-6.py
@@ -1,3 +1,4 @@
+import math
 print("____________________________________________");
 print("Task #1");
 
@@ -41,3 +42,51 @@ while i<6:
 
 print("____________________________________________");
 print("Task #3");
+a=15;
+b=20;
+c=30;
+print("a=",a);
+print("b=",b);
+print("c=",c);
+print("Result of (a+b)/12*c%4+b is:", (a+b)/12*c%4+b)
+print("End of Task #3");
+i = 1
+while i<6:
+    print("*");
+    i=i+1;
+    if i==6:
+        break
+print("____________________________________________");
+print("Task #4");
+a=15;
+b=20;
+c=30;
+print("a=",a);
+print("b=",b);
+print("c=",c);
+print("Result of (a-b*c)/(a+b)%c is:", round((((a-b*c)/(a+b)%c)),5));
+print("End of Task #4");
+i = 1
+while i<6:
+    print("*");
+    i=i+1;
+    if i==6:
+        break
+print("____________________________________________");
+print("Task #5");
+a=25;
+b=-100;
+c=60;
+print("a=",a);
+print("b=",b);
+print("c=",c);
+print("Result of |a-b|/(a+b)3-cos(c)is:", round(((math.fabs(a-b))/(math.fabs(a+b))**3-math.cos(c)),5));
+print("End of Task #5");
+i = 1
+while i<6:
+    print("*");
+    i=i+1;
+    if i==6:
+        break
+print("____________________________________________");
+print("Task #6");
