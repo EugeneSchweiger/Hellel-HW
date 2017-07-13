70aeb5f9a1559ac0129c240741744718cdd55dac
otisLITS <otis01990@gmail.com>
2017-07-13 12:36:35 +0300

    minor changes

--- Task9.py
+++ Task9.py
@@ -4,11 +4,7 @@ print("___________________________________")
 print("Input text block")
 txt1=input()
 print("You have entered:", txt1)
-txt2 = txt1.split(" ")
-txt3=txt2.pop(1)
-txt2.insert(1,txt3.upper())
-str=" ".join(txt2)
-print("Result of this task is:" ,str) #И вообще я ленивый и не буду ничего переделывать
-#Да,можно было бы конечно выделить второй элемент списка,создать усливие заммены всех
-#букв с прописных на заглавные по номерам юникода,но зачем?!
-#Всё и так работает :-)
+Txt_List = txt1.split(" ")
+Txt_List[1]=Txt_List[1].upper()
+str=" ".join(Txt_List)
+print("Result of this task is:" ,Txt_List)
--- task8.py
+++ task8.py
@@ -2,15 +2,12 @@ import math
 print("___________________________________")
 print("Task8")
 print("___________________________________")
-print("Input first text block")
-txt1=input()
-print("Input second text block")
-txt2=input()
+txt1=input("Input first text block")
+txt2=input("Input second text block")
 print("You have entered %s and %s"
       %(txt1,txt2))
-mid1=(round(len(txt1)/2))
-mid2=(round(len(txt2)/2))
+mid1 = len(txt1)//2
+mid2 = len(txt2)//2
 txt3=(txt2[:mid2]+txt1+txt2[mid2:])
 txt4=(txt1[:mid1]+txt3+txt1[mid1:])
 print(txt4)
-#Лень усложнять код,но всё работает) Вроде бы.......
