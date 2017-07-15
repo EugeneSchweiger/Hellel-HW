44e205e2a4d0fc2dbd885a419660b0387ae5c0b4
otisLITS <otis01990@gmail.com>
2017-07-15 21:55:22 +0300

    Nice and easy

---
+++ task14.py
@@ -0,0 +1,11 @@
+number=int(input("Enter some number"))
+def is_even(int):
+    if (int % 2)== 0:
+        return "It's even"
+    else:
+        return "It's odd"
+print(is_even(number))
+#Looks like this task is first training before writing some serious methods
+#so,let's start with it! :-)
+#Code can be inprowed with checking for str-input, for float numbers and hex numbers
+#Please let me know if this is necessary.
