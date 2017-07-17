e3752205e10701fda1cd732c87ef063436ddef1a
otisLITS <otis01990@gmail.com>
2017-07-17 10:54:15 +0300

    Close enought!

--- task14.py
+++ task14.py
@@ -1,3 +1,2 @@
-number=int(input("Enter some number.Let's check,is it even."))
-even=lambda a: True if not a%2 else False
-print(even(number))
+even=lambda a=int(input("Enter some number.Let's check,is it even.")): True if not a%2 else False
+print(even())
