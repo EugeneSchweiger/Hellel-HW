e300565dd27d51474cd1daf1ea368e12a2fc0b0a
otisLITS <otis01990@gmail.com>
2017-07-17 09:53:47 +0300

    1-line function

---
+++ Task14.Ver2.0.py
@@ -0,0 +1,13 @@
+number = int(input("Enter some number.Let's check,is it even."))
+
+
+def even(a):
+    return a % 2
+
+
+if even(number) == 0:
+    print("Even")
+else:
+    print("odd")
+#One-line method) Lol ^_^
+#Thruly I don't know how to do simplier....
