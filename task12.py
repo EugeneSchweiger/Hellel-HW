66a56180854a6fb40b5d8be30c3ce61afb1e770c
otisLITS <otis01990@gmail.com>
2017-07-17 14:21:50 +0300

    minor changes

--- task12.py
+++ task12.py
@@ -1,10 +1,18 @@
 number=int(input("Enter 3-digit number"))
 def int_sum(a):
-    int1=a%10
-    int2=(a%100)//10
-    int3=a//100
-    sum=int1+int2+int3
-    return sum
+    if not (100<=number<=999):
+        return "incorrect number!"
+    else:
+        int1 = a % 10
+        int2 = (a % 100) // 10
+        int3 = a // 100
+        sum = int1 + int2 + int3
+        return sum
+
+
+
+
+
 print("Sum of digits in number,you entered is:" ,int_sum(number))
 
 
