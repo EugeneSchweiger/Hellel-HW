fc7b86f6a467588c7b530f9bf075a77225012204
otisLITS <otis01990@gmail.com>
2017-07-17 10:45:13 +0300

    Checkmate

--- task14.py
+++ task14.py
@@ -1,7 +1,3 @@
 number=int(input("Enter some number.Let's check,is it even."))
-def is_even(a):
-    if not a%2:
-        return True
-    else:
-        return False
-print(is_even(number))
+even=lambda a: True if not a%2 else False
+print(even(number))
