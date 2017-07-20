7cc67a2b38b31d4f3e2bc95ab446eacd8cdccc44
otisLITS <otis01990@gmail.com>
2017-07-20 15:48:37 +0300

    Corrected.v2

--- Task17.py
+++ Task17.py
@@ -7,19 +7,18 @@ c=int(input("c="))
 
 def sovle_quatro_equa(a,b,c):
     D = (b ** 2) - (4 * a * c)
-    is_D_zero = (D == 0)
-    is_D_lesser = (D < 0)
-    is_D_grater = (D > 0)
-    if is_D_lesser:
+    is_d_zero = (D == 0)
+    is_d_lesser = (D < 0)
+    is_d_grater = (D > 0)
+    if is_d_lesser:
         x="No solutions in usual numbers"
         return x
-    elif is_D_zero:
+    elif is_d_zero:
         x = ((-b) / 2 * a)
         return x
-    elif is_D_grater:
+    elif is_d_grater:
         x1 = round(((-b + math.sqrt(D)) / 2 * a),5)
         x2 = round(((-b - math.sqrt(D)) / 2 * a),5)
         return x1,x2
-    else: None
 print("Solution is:",sovle_quatro_equa(a, b, c))
 
