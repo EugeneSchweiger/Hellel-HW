3f0797df8f2193b521e0ce6509e741387d4251dd
otisLITS <otis01990@gmail.com>
2017-07-20 18:02:34 +0300

    first extra H\W

--- EX_Task1.py
+++ EX_Task1.py
@@ -1,17 +1,17 @@
 #Ex1
 string=input("Enter string")
-def reverse(str):
+def str_reverse1(str):
     st2 = "".join([str[len(str)-a-1] for a in range(len(str))])
     return st2
-print(reverse(string))
+print(str_reverse1(string))
 #Ex2
 string=input("Enter string")
-def reverse(str):
+def str_reverse2(str):
     return str[::-1]
-print(reverse(string))
+print(str_reverse2(string))
 
 
 #same Ex2 but shorter
 string=input("Enter string")
-reverse = lambda str: str[::-1]
-print(reverse(string))
+str_reverse3 = lambda str: str[::-1]
+print(str_reverse3(string))
