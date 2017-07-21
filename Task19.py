3facfda7ba6a82241fe77990f740edb0ee049bc8
otisLITS <otis01990@gmail.com>
2017-07-22 01:38:00 +0300

    Fixed miss-calculation of sum of results

--- Task19.py
+++ Task19.py
@@ -1,17 +1,22 @@
-res = 0
-b=range(0,10**6)
-for i in b:
-    res= (3**i)+res
-    if res>10**6:
-        break
-print(res)
+# res = 0
+# b=range(0,10**6)
+# for i in b:
+#     res= (3**i)+res
+#     if res>10**6:
+#         break
+# print(res)
 
 
-res = 0
-i=i>=0
-while i:
-    res= (3**i)+res
+result = 0
+sum_of_results=0
+i=0
+while True:
+    result = (3 ** i)
     i=i+1
-    if res>10**6:
+    if result<10**6:
+        sum_of_results=sum_of_results+result
+    if result>10**6:
         break
-print(res)
+print(sum_of_results)
+
+
