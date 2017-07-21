b2c305628a739ae747d8d0f6276b313a590e4edd
otisLITS <otis01990@gmail.com>
2017-07-21 10:49:36 +0300

    minor changes.

--- Ex_Task2.py
+++ Ex_Task2.py
@@ -1,9 +1,9 @@
 string=input("Enter something")
 indx=input("Enter index inside \"< >\"")
-def return_something_inside_thml_teg(str,index):
-    indx_open="<"+index+">"
-    indx_close="</"+index+">"
-    str2=str[(str.find(indx_open[:])+3):(str.find(indx_close[:]))]
+def return_something_inside_thml_teg(str,tag_letter):
+    tag_open="<"+tag_letter+">"
+    tag_close="</"+tag_letter+">"
+    str2=str[(str.find(tag_open[:])+len(tag_open)):(str.find(tag_close[:]))]
     return str2
 print(return_something_inside_thml_teg(string,indx))
 
