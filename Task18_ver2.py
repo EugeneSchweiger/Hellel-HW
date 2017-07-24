947d5f37410e1a798922a8fc821b8c02153b43e3
otisLITS <otis01990@gmail.com>
2017-07-25 01:10:56 +0300

    simplier is only with lambdas)

--- Task18_ver2.py
+++ Task18_ver2.py
@@ -2,16 +2,27 @@
 chr1=ord(input("Enter first character end press \"Enter\""))
 chr2=ord(input("Enter second character end press \"Enter\""))
 
-def sum(a, b):
-    if chr1 > chr2:
-        sum = 0
-        for i in range(b, (a + 1)):
-            sum += i
-        return sum
-    else:
-        sum = 0
-        for i in range(a, (b + 1)):
-            sum += i
-        return sum
-print(sum(chr1,chr2))
+def sum(a,b):
+    sum=0
+    for v in range(a,b+1):
+        sum += v
+    print(sum)
+if chr1 > chr2:
+    sum(chr2,chr1)
+else:
+    sum(chr1,chr2)
+
+
+# def sum(a, b):
+#     if chr1 > chr2:
+#         sum = 0
+#         for i in range(b, (a + 1)):
+#             sum += i
+#         return sum
+#     else:
+#         sum = 0
+#         for i in range(a, (b + 1)):
+#             sum += i
+#         return sum
+
 
