a63f1e02b1a57f60eec9240de91ad90cd4675ebd
otisLITS <otis01990@gmail.com>
2017-07-27 00:14:42 +0300

    minor changes

--- Task21.py
+++ Task21.py
@@ -1,27 +1,24 @@
 
 def output_all_nums_with_1_and_7():
     n = 1000
-    lst = []
+    lst1 = []
     count = 0
-    lst2=[]
+    lst2 = []
     lst3 = []
     for i in range(n):
         for char in str(i):
             if char=="1" or char=="7":
-                lst.append(i)
+                lst1.append(i)
                 count+=1
-    for char_2 in lst:
-        for char_1 in str(char_2):
+    for item_1 in lst1:
+        for char_1 in str(item_1):
             if  char_1=="7":
-                lst2.append(char_2)
-    for char_4 in lst2:
-        for char_3 in str(char_4):
-            if  char_3=="1":
-                lst3.append(char_4)
-        ###
-    lst3=list(set(lst3))
-    lst3.sort()
-    return lst3
+                lst2.append(item_1)
+    for item_2 in lst2:
+        for char_2 in str(item_2):
+            if  char_2=="1":
+                lst3.append(item_2)
+    return sorted(list(set(lst3)))
 print(output_all_nums_with_1_and_7())
 """
 Task works correctly,however it can be divided in 3 separated methods:
