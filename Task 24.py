c948e3e307baa804cfaaeb687e45951bc915f590
otisLITS <otis01990@gmail.com>
2017-07-26 23:35:05 +0300

    Game over

---
+++ Task 24.py
@@ -0,0 +1,14 @@
+import random
+rand_int=random.randint(1,10)
+while True:
+    choice = int(input("Now guess an number!(between 1 and 10)"))
+    if choice==rand_int:
+        print("Yep!It's my number")
+        break
+    if 1<=choice<=10:
+        if choice<rand_int:
+            print("Lesser")
+        if choice>rand_int:
+            print("Bigger")
+    else: print("Wrong number")
+    continue
