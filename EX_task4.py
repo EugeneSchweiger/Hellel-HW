58249f13fa6d681ddafee6d7984b6872a8eb99f5
otisLITS <otis01990@gmail.com>
2017-07-27 00:26:17 +0300

    fixed if-statements

--- EX_task4.py
+++ EX_task4.py
@@ -1,7 +1,7 @@
 def daylight_saving(month, week_day, day_of_month):
     m=1<=month<=12
     w=1<=week_day<=7
-    d=1<=week_day<=31
+    d=1<=day_of_month<=31
 
     if m and w and d:
         if month == 3 :
@@ -15,10 +15,13 @@ def daylight_saving(month, week_day, day_of_month):
                 if 24 < day_of_month <= 31:
                     print("clock adjustment need:")
                     return -1
-
         else:
-            print("clock adjustment not needed,or date input incorrect:")
-        return 0
+            print("clock adjustment not needed")
+
+    else:
+        print("date input incorrect")
+
+    return 0
 m=int(input('enter month(1-12)'))
 w=int(input("enter week day(1-7)"))
 d=int(input("enter day itself(1-31)"))
