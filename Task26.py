41e43fda186cad5ffa279a027ebef7275844426b
otisLITS <otis01990@gmail.com>
2017-07-27 03:17:05 +0300

    26 Easy way

--- Task26.py
+++ Task26.py
@@ -1,13 +1,20 @@
-def simple(rang=100):
-    num=1
-    lst=[1]
-    for num in range(rang):
-        if num > 1:
-            for i in range(2, num):
-                if (num % i) == 0:
-                    break
-            else:
-                lst.append(num)
-    return lst
+# def simple(rang=100):
+#     lst=[1]
+#     for num in range(rang):
+#         if num > 1:
+#             for i in range(2, num):
+#                 if (num % i) == 0:
+#                     break
+#             else:
+#                 lst.append(num)
+#     return lst
+#
+# print(simple(200))
 
-print(simple(200))
+"""
+Damn!That was hard!
+"""
+lst = lambda rang: [num for num in range(2, rang) if not 0 in map(lambda z: num % z, range(2,num))]
+print(", ".join(map(str, lst(200))))
+
+
