adcf4692517eccfcc5045a784978b3f6d0998158
otisLITS <otis01990@gmail.com>
2017-07-29 22:53:57 +0300

    Task27 randomizer is better

--- edu/hillel/homework/Task27.py
+++ edu/hillel/homework/Task27.py
@@ -1,7 +1,7 @@
 import random
 
 def fill_wiz_odd(list,rang=100):
-    while len(list)<(rang/2) :
+    while len(list)<(rang//2) :
         for num in range(rang):
             if num%2==0:
                 continue
@@ -13,7 +13,7 @@ def lst_2_str(list):
     return list
 
 lst=[]
-fill_wiz_odd(lst)
+fill_wiz_odd(lst,10000)
 print(lst_2_str(lst))
 
 def list_index_randomizer(lst):
@@ -25,4 +25,24 @@ def list_index_randomizer(lst):
         i+=1
     return lst
 
-print(lst_2_str(list_index_randomizer(lst)))
+
+def my_shuffle(lst):
+    for i in range(len(lst)):
+        idx1 = random.randint(0, len(lst)-1)
+        idx2 = random.randint(0, len(lst)-1)
+        lst[idx1], lst[idx2] = lst[idx2], lst[idx1]
+    return lst
+
+def foo(lst1,lst2):
+    count=0
+    for i in range(len(lst1)):
+        if lst1[i]==lst2[i]:
+            count+=1
+    return  count, "indexes on their old place"
+
+
+
+# print(lst_2_str(my_shuffle(lst)),"shuffle",)
+# print(lst_2_str(list_index_randomizer(lst)),)
+print("Shuffle",foo(list(lst),list(my_shuffle(lst))))
+print("IDX randomizer",foo(list(lst),list(list_index_randomizer(lst))))
