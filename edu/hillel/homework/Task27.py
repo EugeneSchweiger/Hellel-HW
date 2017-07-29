73148cbecbbf84dcf56545971681bce86c0f4c82
otisLITS <otis01990@gmail.com>
2017-07-29 12:57:35 +0300

    Task27 without rand.shuffle

--- edu/hillel/homework/Task27.py
+++ edu/hillel/homework/Task27.py
@@ -15,7 +15,15 @@ def lst_2_str(list):
 lst=[]
 fill_wiz_odd(lst)
 print(lst)
-string_list=lst_2_str(lst)
-random.shuffle(lst)
-print(string_list)
-print(lst_2_str(lst))
+
+def foo(lst):
+    choise_IDs = [i for i in range(len(lst))]
+    lst2=[]
+    i=0
+    while i< len(lst):
+        choise=int(random.choice(choise_IDs))
+        lst2.append(lst[choise_IDs.pop(choise_IDs.index(choise))])
+        i+=1
+    return lst2
+
+print(foo(lst))
