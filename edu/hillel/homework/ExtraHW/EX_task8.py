d741f38fd26a2aaab54a41d516e25b4662c389ab
otisLITS <otis01990@gmail.com>
2017-08-02 15:10:27 +0300

    EX_task8 lower-uppercase fix

--- edu/hillel/homework/ExtraHW/EX_task8.py
+++ edu/hillel/homework/ExtraHW/EX_task8.py
@@ -8,24 +8,24 @@ def is_pangram(txt):
     if len(txt)<=25:
         return False
     else:
-        txt="".join(list(set(txt)))
+        txt="".join(list(set(txt.lower())))
         for i in range(len(txt)):
-            if len(txt)<25:       
+            if len(txt)<25:
                 return False
             if  txt[i] in alphabet:
                 letters_count+=1
         if letters_count==26:
             return True
         else: return False
-"""
-To speed up code working I add 2 length checkings
-First check owerall input text length. If it's small-expecting result
-Second check only unique symbols. If they are not 26- also expecting result.
-However printeble symbols count from keyboard is not so big 
-"""
 
+import timeit
+def test():
+    return is_pangram(input_text)
+
+print("overall time",timeit.timeit("test()", setup="from __main__ import test", number=1),"seconds")
 
 if is_pangram(input_text):
     print("It IS pangram")
 else:
     print("It's NOT pangram")
+
