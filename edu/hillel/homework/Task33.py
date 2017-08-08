a1a14d0ad61e5f1aaa19351c6ba1174c7a8673da
otisLITS <otis01990@gmail.com>
2017-08-09 02:29:51 +0300

    Task33

---
+++ edu/hillel/homework/Task33.py
@@ -0,0 +1,136 @@
+group = [
+    {"id": 1024, "fullname": "Александр Скворцов",    "email": "", "github": "https://github.com/Agskvortsov", "rank": 0},
+    {"id": 1025, "fullname": "Андрей Рожко",          "email": "", "github": "https://github.com/rozhkoandrew", "rank": 0},
+    {"id": 1026, "fullname": "АлексейКулишенко ",     "email": "", "github": "https://github.com/", "rank": 0},
+    {"id": 1027, "fullname": "Виталий Рыжков",        "email": "", "github": "https://github.com/vitalyryzhkov", "rank": 0},
+    {"id": 1028, "fullname": "Виталина Гавеля",       "email": "", "github": "https://github.com/", "rank": 0},
+    {"id": 1029, "fullname": "Виктор Бурлаков",       "email": "", "github": "https://github.com/SancheeZzz", "rank": 0},
+    {"id": 1030, "fullname": "Виктор Горовой",        "email": "", "github": "https://github.com/lamboosanproject", "rank": 0},
+    {"id": 1031, "fullname": "Надежда Симанович",     "email": "", "github": "https://github.com/simanadya", "rank": 0},
+    {"id": 1032, "fullname": "Николай Марушевский",   "email": "", "github": "https://github.com/Gelios-Sky", "rank": 0},
+    {"id": 1033, "fullname": "Андрей Кравчук",        "email": "", "github": "https://github.com/albaderon", "rank": 0},
+    {"id": 1034, "fullname": "Екатерина Шадрина",     "email": "", "github": "https://github.com/katefeline", "rank": 0},
+    {"id": 1035, "fullname": "Александр Малышев",     "email": "", "github": "https://github.com/Barabasha", "rank": 0},
+    {"id": 1036, "fullname": "Владимир Веренчук",     "email": "", "github": "https://github.com/", "rank": 0}
+]
+
+hw_results = [
+    {"id": 1024, "task_completion": [1,1,1,1,1,1,1,0,1,0,1,0,0,1,0,1,1,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1025, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0] },
+    {"id": 1026, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1027, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0] },
+    {"id": 1028, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1029, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0] },
+    {"id": 1030, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1031, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1032, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1033, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0] },
+    {"id": 1034, "task_completion": [1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1035, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0] },
+    {"id": 1036, "task_completion": [1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] }
+]
+
+
+test1_results = [
+    {"id": 1024, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1025, "task_completion": [1,1,1,1,1,1,1,1,1,0,0,1] },
+    {"id": 1026, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1027, "task_completion": [1,1,1,1,1,1,1,1,0,0,0,0] },
+    {"id": 1028, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1029, "task_completion": [1,1,1,1,1,0,0,0,0,0,0,0] },
+    {"id": 1030, "task_completion": [1,1,1,1,1,1,1,1,0,0,0,0] },
+    {"id": 1031, "task_completion": [1,1,1,1,0,0,0,0,0,0,0,0] },
+    {"id": 1032, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1033, "task_completion": [1,1,1,1,1,1,1,1,1,0,0,1] },
+    {"id": 1034, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1035, "task_completion": [1,1,1,1,1,1,1,1,1,0,1,1] },
+    {"id": 1036, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] }
+]
+
+test1_weights = {
+    1 : 1,
+    2 : 1,
+    3 : 1,
+    4 : 2,
+    5 : 2,
+    6 : 2,
+    7 : 4,
+    8 : 4,
+    9 : 4,
+    10 : 8,
+    11 : 8,
+    12 : 15
+}
+
+
+def update_students_results(group):
+    for i in range(len(test1_results)):
+        for x in range(len(test1_weights)):
+            test1_results[i]["task_completion"][x] *= test1_weights[x+1]
+
+    for stud_hw in group:
+        for hw_result in hw_results:
+            if stud_hw["id"] == hw_result["id"]:
+                stud_hw["rank"] = sum(hw_result["task_completion"])
+
+    for stud_test in group:
+        for hw_result in test1_results:
+            if stud_test['id'] == hw_result['id']:
+                stud_test['rank'] += sum(hw_result['task_completion'])
+    return group
+
+
+def print_students_info(group):
+    for person in group:
+        print("_" * 60)
+        print("| ID:         %40s |" % person['id'])
+        print("-" * 60)
+        print("| Full name:  %40s |" % person['fullname'])
+        print("| Email:      %40s |" % person['email'])
+        print("| Github:     %40s |" % person['github'])
+        print("| Rank:       %40s |" % person['rank'])
+        print("_"*60)
+
+
+def print_sorted_students_info(group, sort_by_key="rank"):
+    if sort_by_key!="rank":
+        group.sort(key=lambda group: group.get(sort_by_key))
+        print_students_info(group)
+    else:
+        group.sort(key=lambda group: group.get(sort_by_key), reverse=True)
+        print_students_info(group)
+
+def main():
+    while True:
+        try:
+            print ()
+            print ("~ Welcome to Students book ~")
+            print ("Select one of actions below:")
+            print ("     1 - Print students cards sorted by name")
+            print ("     2 - Print students cards sorted by rank")
+            print ("     3 - Print students cards sorted by ID")
+            print ("-----------------------------")
+            print ("     0 - Exit")
+
+            user_input = input("Enter you choice: ")
+            choice = int(user_input)
+
+            if choice == 1:
+                print_sorted_students_info(group, "fullname")
+            elif choice == 2:
+                print_sorted_students_info(group)
+            elif choice == 3:
+                print_sorted_students_info(group, "id")
+            elif choice == 0:
+                print ("Bye!")
+                break
+            else:
+                print ("Chose one of them!")
+        except ValueError:# I'm still not familiar with exceptions
+                print("Something went wrong. Try again...")
+
+
+
+if __name__ == "__main__":
+    update_students_results(group)
+    main()
