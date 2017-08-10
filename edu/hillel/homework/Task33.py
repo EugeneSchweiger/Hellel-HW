d9c5614ab45446a5bbe7fec118302b134d53adfa
otisLITS <otis01990@gmail.com>
2017-08-11 02:10:06 +0300

    Task33 update(pull info from url)

--- edu/hillel/homework/Task33.py
+++ edu/hillel/homework/Task33.py
@@ -1,78 +1,224 @@
-group = [
-    {"id": 1024, "fullname": "Тимченко Дмитрий",    "email": "dmt.tym@gmail.com", "github": "https://github.com/TymDmitriy", "rank": 0},
-    {"id": 1025, "fullname": "Юношев Павел",        "email": "p.n.yunoshev@gmail.com", "github": "", "rank": 0},
-    {"id": 1026, "fullname": "Лукшин Евгений",      "email": "otis01990@gmail.com", "github": "https://github.com/EugeneSchweiger", "rank": 0},
-    {"id": 1027, "fullname": "Сеченова Анна",       "email": "sechenovaanna@gmail.com", "github": "https://github.com/AnnaSechenova", "rank": 0},
-    {"id": 1028, "fullname": "Квято Сергей",        "email": "skvantos@gmail.com", "github": "https://github.com/kvantos/", "rank": 0},
-    {"id": 1029, "fullname": "Кань Евгений",        "email": "suckrat.us1337@gmail.com", "github": "https://github.com/suckratus", "rank": 0},
-    {"id": 1030, "fullname": "Лавренко Евгений",    "email": "superlavrik@gmail.com", "github": "https://github.com/SuperLavrik", "rank": 0},
-    {"id": 1031, "fullname": "Кирсанов Илья",       "email": "ilya.kirsanov@gmail.com", "github": "https://github.com/IlyaKirsanov", "rank": 0},
-    {"id": 1032, "fullname": "Жолондковский Вадим", "email": "vadymzholondkovskiy@gmail.com", "github": "", "rank": 0},
-    {"id": 1033, "fullname": "Марченко Вадим",      "email": "wardomir@gmail.com", "github": "", "rank": 0},
-    {"id": 1034, "fullname": "Митев Алексей",       "email": "oleksii.mitiev@gmail.com", "github": "https://github.com/omitiev", "rank": 0},
-    {"id": 1035, "fullname": "Якутко Анастасия",    "email": "anastasiia.yakutko@gmail.com", "github": "https://github.com/ayakutko/HW_1_6", "rank": 0},
-    {"id": 1036, "fullname": "Каменцев Никита",     "email": "niqkamentsev@gmail.com", "github": "https://github.com/TheNiq", "rank": 0},
-    {"id": 1037, "fullname": "Белоус Екатерина",    "email": "katherinebilous@gmail.com", "github": "https://github.com/KateBilous/", "rank": 0},
-    {"id": 1038, "fullname": "Друмов Вадим",        "email": "vkdrumov@gmail.com", "github": "https://github.com/Vkdrumov", "rank": 0},
-]
+# group = [
+#     {"id": 1024, "fullname": "Тимченко Дмитрий",    "email": "dmt.tym@gmail.com", "github": "https://github.com/TymDmitriy", "rank": 0},
+#     {"id": 1025, "fullname": "Юношев Павел",        "email": "p.n.yunoshev@gmail.com", "github": "", "rank": 0},
+#     {"id": 1026, "fullname": "Лукшин Евгений",      "email": "otis01990@gmail.com", "github": "https://github.com/EugeneSchweiger", "rank": 0},
+#     {"id": 1027, "fullname": "Сеченова Анна",       "email": "sechenovaanna@gmail.com", "github": "https://github.com/AnnaSechenova", "rank": 0},
+#     {"id": 1028, "fullname": "Квято Сергей",        "email": "skvantos@gmail.com", "github": "https://github.com/kvantos/", "rank": 0},
+#     {"id": 1029, "fullname": "Кань Евгений",        "email": "suckrat.us1337@gmail.com", "github": "https://github.com/suckratus", "rank": 0},
+#     {"id": 1030, "fullname": "Лавренко Евгений",    "email": "superlavrik@gmail.com", "github": "https://github.com/SuperLavrik", "rank": 0},
+#     {"id": 1031, "fullname": "Кирсанов Илья",       "email": "ilya.kirsanov@gmail.com", "github": "https://github.com/IlyaKirsanov", "rank": 0},
+#     {"id": 1032, "fullname": "Жолондковский Вадим", "email": "vadymzholondkovskiy@gmail.com", "github": "", "rank": 0},
+#     {"id": 1033, "fullname": "Марченко Вадим",      "email": "wardomir@gmail.com", "github": "", "rank": 0},
+#     {"id": 1034, "fullname": "Митев Алексей",       "email": "oleksii.mitiev@gmail.com", "github": "https://github.com/omitiev", "rank": 0},
+#     {"id": 1035, "fullname": "Якутко Анастасия",    "email": "anastasiia.yakutko@gmail.com", "github": "https://github.com/ayakutko/HW_1_6", "rank": 0},
+#     {"id": 1036, "fullname": "Каменцев Никита",     "email": "niqkamentsev@gmail.com", "github": "https://github.com/TheNiq", "rank": 0},
+#     {"id": 1037, "fullname": "Белоус Екатерина",    "email": "katherinebilous@gmail.com", "github": "https://github.com/KateBilous/", "rank": 0},
+#     {"id": 1038, "fullname": "Друмов Вадим",        "email": "vkdrumov@gmail.com", "github": "https://github.com/Vkdrumov", "rank": 0},
+# ]
+#
+# hw_results = [
+#     {"id":1024, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0]},
+#     {"id":1025, "task_completion": [1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+#     {"id":1026, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0]},
+#     {"id":1027, "task_completion": [1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+#     {"id":1028, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0]},
+#     {"id":1029, "task_completion": [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+#     {"id":1030, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0]},
+#     {"id":1031, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]},
+#     {"id":1032, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+#     {"id":1033, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0]},
+#     {"id":1034, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]},
+#     {"id":1035, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+#     {"id":1036, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,0]},
+#     {"id":1037, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+#     {"id":1038, "task_completion": [1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+# ]
+#
+#
+# test1_results = [
+#     {"id": 1024, "task_completion": [1,1,1,1,1,1,1,1,1,1,0,0] },
+#     {"id": 1025, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+#     {"id": 1026, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1] },
+#     {"id": 1027, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+#     {"id": 1028, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1] },
+#     {"id": 1029, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+#     {"id": 1030, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,1] },
+#     {"id": 1031, "task_completion": [1,1,1,1,1,1,0,0,0,0,0,0] },
+#     {"id": 1032, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+#     {"id": 1033, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,0] },
+#     {"id": 1034, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+#     {"id": 1035, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+#     {"id": 1036, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+#     {"id": 1037, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+#     {"id": 1038, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+# ]
+#
+# test1_weights = {
+#     1 : 1,
+#     2 : 1,
+#     3 : 1,
+#     4 : 2,
+#     5 : 2,
+#     6 : 2,
+#     7 : 4,
+#     8 : 4,
+#     9 : 4,
+#     10 : 8,
+#     11 : 8,
+#     12 : 15
+# }
+#
+#
+# def update_students_results(group):
+#     for i in range(len(test1_results)):
+#         for x in range(len(test1_weights)):
+#             test1_results[i]["task_completion"][x] *= test1_weights[x+1]
+#
+#     for stud_hw in group:
+#         for hw_result in hw_results:
+#             if stud_hw["id"] == hw_result["id"]:
+#                 stud_hw["rank"] = sum(hw_result["task_completion"])
+#
+#     for stud_test in group:
+#         for test_result in test1_results:
+#             if stud_test['id'] == test_result['id']:
+#                 stud_test['rank'] += sum(test_result['task_completion'])
+#     return group
+#
+#
+# def print_students_info(group):
+#     for person in group:
+#         print("_" * 60)
+#         print("| ID:         %40s |" % person['id'])
+#         print("-" * 60)
+#         print("| Full name:  %40s |" % person['fullname'])
+#         print("| Email:      %40s |" % person['email'])
+#         print("| Github:     %40s |" % person['github'])
+#         print("| Rank:       %40s |" % person['rank'])
+#         print("_"*60)
+#
+#
+# def print_sorted_students_info(group, sort_by_key="rank"):
+#     if sort_by_key!="rank":
+#         group.sort(key=lambda group: group.get(sort_by_key))
+#         print_students_info(group)
+#     else:
+#         group.sort(key=lambda group: group.get(sort_by_key), reverse=True)
+#         print_students_info(group)
+#
+# def main():
+#     while True:
+#         try:
+#             print ()
+#             print ("~ Welcome to Students book ~")
+#             print ("Select one of actions below:")
+#             print ("     1 - Print students cards sorted by name")
+#             print ("     2 - Print students cards sorted by rank")
+#             print ("     3 - Print students cards sorted by ID")
+#             print ("-----------------------------")
+#             print ("     0 - Exit")
+#
+#             user_input = input("Enter you choice: ")
+#             choice = int(user_input)
+#
+#             if choice == 1:
+#                 print_sorted_students_info(group, "fullname")
+#             elif choice == 2:
+#                 print_sorted_students_info(group)
+#             elif choice == 3:
+#                 print_sorted_students_info(group, "id")
+#             elif choice == 0:
+#                 print ("Bye!")
+#                 break
+#             else:
+#                 print ("Chose one of them!")
+#         except ValueError:# I'm still not familiar with exceptions
+#                 print("Something went wrong. Try again...")
+#
+#
+#
+# if __name__ == "__main__":
+#     update_students_results(group)
+#     main()
+#
+import requests
+import json
+from pprint import pprint as pp
+BASE_URL = "http://54.201.47.219:8080/api"
+VERSION  = "v1"
+URL = "%s/%s" % (BASE_URL, VERSION)
+def  get_hw_results():
+    response = requests.get(URL+'/hw_results/')
+    if response.status_code == 200:
+        json_object = response.json()
+        return json_object
+    else:
+        log_error(response.content)
 
-hw_results = [
-    {"id":1024, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0]},
-    {"id":1025, "task_completion": [1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
-    {"id":1026, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0]},
-    {"id":1027, "task_completion": [1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
-    {"id":1028, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0]},
-    {"id":1029, "task_completion": [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
-    {"id":1030, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0]},
-    {"id":1031, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]},
-    {"id":1032, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
-    {"id":1033, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0]},
-    {"id":1034, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]},
-    {"id":1035, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
-    {"id":1036, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,0]},
-    {"id":1037, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
-    {"id":1038, "task_completion": [1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
-]
+
+def get_tests_results():
+    response = requests.get(URL+'/test1_results/')
+    if response.status_code == 200:
+        json_object = response.json()
+        return json_object
+    else:
+        log_error(response.content)
+
+def get_weights():
+    response = requests.get(URL + '/test1_weights/')
+    if response.status_code == 200:
+        json_object = response.json()
+        weights = json_object['test1_weights'][:]
+    return weights
+
+def get_students():
+    response = requests.get(URL + '/students')
+    if response.status_code == 200:
+        json_object = response.json()
+        return (json_object['students'])
+    else:
+        log_error(response.content)
 
 
-test1_results = [
-    {"id": 1024, "task_completion": [1,1,1,1,1,1,1,1,1,1,0,0] },
-    {"id": 1025, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
-    {"id": 1026, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1] },
-    {"id": 1027, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
-    {"id": 1028, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1] },
-    {"id": 1029, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
-    {"id": 1030, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,1] },
-    {"id": 1031, "task_completion": [1,1,1,1,1,1,0,0,0,0,0,0] },
-    {"id": 1032, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
-    {"id": 1033, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,0] },
-    {"id": 1034, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
-    {"id": 1035, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
-    {"id": 1036, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
-    {"id": 1037, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
-    {"id": 1038, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
-]
+def get_student_by_id(id):
+    response = requests.get(URL+ '/students/' + str(id))
+    if response.status_code == 200:
+        json_object = response.json()
+        return json_object['student']
+    else:
+        log_error(response.content)
 
-test1_weights = {
-    1 : 1,
-    2 : 1,
-    3 : 1,
-    4 : 2,
-    5 : 2,
-    6 : 2,
-    7 : 4,
-    8 : 4,
-    9 : 4,
-    10 : 8,
-    11 : 8,
-    12 : 15
-}
+#
+# def update_student(id, upd_fields):
+#     response = requests.put(URL + '/students/' + str(id),
+#                             json=json.dumps(upd_fields))
+#
+#     result = response.status_code == 200
+#
+#     if not result:
+#         log_error(response.content)
+#
+#     return result
+#
+# def add_student(student):
+#     response = requests.post(URL + '/students/',
+#                             json=json.dumps(student))
+#
+#     result = response.status_code == 200
+#
+#     if not result:
+#         log_error(response.content)
+#
+#     return result
+
+def log_error(msg):
+        print("ERROR: ", msg)
 
 
 def update_students_results(group):
     for i in range(len(test1_results)):
         for x in range(len(test1_weights)):
-            test1_results[i]["task_completion"][x] *= test1_weights[x+1]
+            test1_results[i]["task_completion"][x] *= test1_weights[x]
 
     for stud_hw in group:
         for hw_result in hw_results:
@@ -106,6 +252,8 @@ def print_sorted_students_info(group, sort_by_key="rank"):
         group.sort(key=lambda group: group.get(sort_by_key), reverse=True)
         print_students_info(group)
 
+
+
 def main():
     while True:
         try:
@@ -115,6 +263,7 @@ def main():
             print ("     1 - Print students cards sorted by name")
             print ("     2 - Print students cards sorted by rank")
             print ("     3 - Print students cards sorted by ID")
+            print ("     4 - Print student cards finded by ID")
             print ("-----------------------------")
             print ("     0 - Exit")
 
@@ -127,17 +276,22 @@ def main():
                 print_sorted_students_info(group)
             elif choice == 3:
                 print_sorted_students_info(group, "id")
+            elif choice==4:
+                pp(get_student_by_id(int(input("enter ID"))))
             elif choice == 0:
                 print ("Bye!")
                 break
             else:
                 print ("Chose one of them!")
-        except ValueError:# I'm still not familiar with exceptions
+        except ValueError:
                 print("Something went wrong. Try again...")
 
 
 
 if __name__ == "__main__":
+    group = get_students()
+    test1_results = get_tests_results()
+    test1_weights = get_weights()
+    hw_results = get_hw_results()
     update_students_results(group)
     main()
-
